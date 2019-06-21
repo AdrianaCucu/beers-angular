588bb2edd170a220fe91fb57234606d4223a885d
Adriana <adrianacucu99@gmail.com>
2019-06-21 09:00:07 +0100

    Fixed duplicate favourites bug.

--- src/app/services/favourites.service.ts
+++ src/app/services/favourites.service.ts
@@ -5,11 +5,31 @@ import { Injectable } from '@angular/core';
 })
 export class FavouritesService {
   items = new Set();
+  ids = [];
 
   constructor() {}
 
+  /**
+   * Updates the array of ids and the set of favourites.
+   * (Needed ids array to solve duplicates issue.)
+   *
+   * @param beer
+   */
   addToFavourites(beer) {
-    this.items.add(beer);
+    let exists = false;
+    for (let i = 0; i < this.ids.length && !exists; i++) {
+      if (beer.id === this.ids[i]) {
+        exists = true;
+      }
+    }
+
+    if (!exists) {
+      this.ids.push(beer.id);
+      this.items.add(beer);
+    }
+
+    console.log(this.ids);
+    console.log(this.items);
   }
 
   removeFromFavourites(beer) {
