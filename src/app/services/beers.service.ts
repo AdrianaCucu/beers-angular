85f682edd0b5e44c7a9a22e823e2f76b9baa20ca
Adriana <adrianacucu99@gmail.com>
2019-06-24 11:48:02 +0100

    Fixed beer list display bug.

--- src/app/services/beers.service.ts
+++ src/app/services/beers.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class BeersService {
   API_PATH = 'https://api.punkapi.com/v2';
-  MAX_PER_PAGE = 40;
+  MAX_PER_PAGE = 30;
 
   selectedBeerId;
   filter = 'no-filter';
