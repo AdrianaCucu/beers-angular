967f9b91905cf384118d6d026629dbaccac39ebf
Adriana <adrianacucu99@gmail.com>
2019-06-26 12:24:15 +0100

    Updated favourites to use array instead of set.

--- src/app/services/favourites.service.ts
+++ src/app/services/favourites.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FavouritesService {
-  items = new Set();
+  items = [];
   ids = [];
 
   constructor() {}
@@ -23,11 +23,16 @@ export class FavouritesService {
 
     if (!exists) {
       this.ids.push(beer.id);
-      this.items.add(beer);
+      this.items.push(beer);
     }
 
     console.log(this.ids);
     console.log(this.items);
+
+    localStorage.setItem('favourites', JSON.stringify(this.items));
+    let storage = localStorage.getItem('favourites');
+    console.log('storage');
+    console.log(storage);
   }
 
   /**
@@ -37,9 +42,9 @@ export class FavouritesService {
     for (let i = 0; i < this.ids.length; i++) {
       if (beer.id === this.ids[i]) {
         this.ids.splice(i, 1);
+        this.items.splice(i, 1);
       }
     }
-    this.items.delete(beer);
   }
 
   getItems() {
@@ -51,7 +56,7 @@ export class FavouritesService {
    */
   clearFavourites() {
     this.ids = [];
-    this.items = new Set();
+    this.items = [];
     return this.items;
   }
 }
