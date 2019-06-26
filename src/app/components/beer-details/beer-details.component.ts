4e203f218461af13d05cf6751f79fa6b791df7f0
Adriana <adrianacucu99@gmail.com>
2019-06-26 11:02:39 +0100

    Updated beer details routing.

--- src/app/components/beer-details/beer-details.component.ts
+++ src/app/components/beer-details/beer-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { FavouritesService } from 'src/app/services/favourites.service';
 import { BeersService } from 'src/app/services/beers.service';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-beer-details',
@@ -16,20 +17,38 @@ export class BeerDetailsComponent implements OnInit {
   constructor(
     private favouritesService: FavouritesService,
     private beersService: BeersService,
-    private router: Router
-  ) {}
+    private route: ActivatedRoute
+  ) {
+    route.params.subscribe();
+    this.id = route.params.value.beerId;
+  }
 
   ngOnInit() {
-    this.id = this.router.url.replace(/\D/g, '');
+    // this.id = this.router.url.replace(/\D/g, '');
     this.beersService.setBeerById(this.id);
     console.log(this.id);
     this.updatePage();
   }
 
+  /**
+   * Checks that the id is a positive integer between 1 and 325.
+   *
+   * @param id - the beer id contained in the route
+   */
+  validateId(id: string) {
+    return (
+      /^\d+$/.test(this.id) &&
+      parseInt(this.id, 10) > 0 &&
+      parseInt(this.id, 10) < 326
+    );
+  }
+
   updatePage() {
-    this.beersService
-      .getBeers(1, 'selected')
-      .subscribe(item => (this.items = item));
+    if (this.validateId(this.id)) {
+      this.beersService
+        .getBeers(1, 'selected')
+        .subscribe(item => (this.items = item));
+    }
   }
 
   addToFavourites(beer) {
